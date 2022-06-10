99f9bfe1cca14d913ce2bd3f6a0bbb2e0aba64d2
Carlos Bessa <48933250+Bessa1@users.noreply.github.com>
2022-06-10 09:41:55 -0300

    Update Atv7.java

--- atvjava1/Atv7.java
+++ atvjava1/Atv7.java
@@ -1,54 +1,40 @@
-//Faça um programa que leia  3 valores 
-//(considere que não serão informados valores iguais)
-// e escrever a soma dos 2 maiores. 
-
 import java.util.Scanner;
 
-    public class Atv7 {
-    public static void main(String[] args) {
+//Faça um programa que leia  3 valores (considere que não 
+//serão informados valores iguais) e escrever a soma dos 2 maiores. 
 
-     int soma = 0; 
-      Scanner in = new Scanner( System.in );
-      
-      System.out.println("Informe o valor 1: ");
-      int valor1 = in.nextInt();
+public class Atv7{
+public static void main(String[] args){
 
-      System.out.println("Informe o valor 2: ");
-      int valor2 = in.nextInt();
+int soma = 0;
 
-      System.out.println("Informe o valor 3: ");
-      int valor3 = in.nextInt();
+Scanner in = new Scanner(System.in);
 
-       
-        if(valor1 < valor2 && valor1 < valor3){
-             soma = valor2 + valor3;
-           System.out.println("A soma dos maiores valores é:" + soma);
-          
-       }
-        if(valor2 < valor1 && valor2 > valor3){
-             soma = valor1 + valor3;
-           System.out.println("A soma dos maiores valores é:" + soma);
-          
-       }
+System.out.println("Digite o primeiro valor: ");
+int valor1 = in.nextInt();
+System.out.println("Digiste o segundo valor: ");
+int valor2 = in.nextInt();
+System.out.println("Digite o terceiro valor: ");
+int valor3 = in.nextInt();
 
-        if(valor3 < valor2 && valor3 > valor1){
-             soma = valor1 + valor2;
-           System.out.println("A soma dos maiores valores é: " + soma);
-          
-       }
 
-        if(valor1 == valor2 && valor1 == valor3){
-           System.out.println("Você digitou números iguais,Informe o primeiro valor novamente:");
-       }
-        if(valor2 == valor1 && valor2 == valor3){
-           System.out.println("Você digitou números iguais, Informe o segundo valor novamente:");
-       }
 
-        if(valor3 == valor2 && valor3 == valor1){
-           System.out.println("Você digitou números iguais, Informe o terceiro valor novamente:");
-       }
+if(valor1 < valor2 && valor1 < valor3){
+    soma = valor2 + valor3;
+    System.out.println("A soma dos maiores valores são: " + soma);
 
-        
-      }
+    
+}
+else if(valor2 < valor1 && valor2 < valor3){
+    soma = valor1 + valor3;
+     System.out.println("A soma dos maiores valores são: " + soma);
+}
+else{
+    soma = valor1 + valor2;
+     System.out.println("A soma dos maiores valores são: " + soma);
+}
 
-    }
+}
+
+
+}
